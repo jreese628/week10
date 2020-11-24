9aa7a22e864c3313dc93a9b7006cb5153d81691c
jreese628 <72086483+jreese628@users.noreply.github.com>
2020-11-23 23:40:37 -0500

    Add files via upload

---
+++ week10/easy/script.js
@@ -0,0 +1,8 @@
+function average(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+}
+console.log(average([9, 7, 5,]));
---
+++ week10/hard/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.button1').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'red';
+})
+
+document.querySelector('.button2').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = 'white';
+})
